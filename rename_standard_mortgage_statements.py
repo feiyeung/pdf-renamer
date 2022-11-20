6838c59bf89732b0632b23cad371d1c4e58911fb
Feiyang Xue <hi@fxue.dev>
2022-11-19 23:01:42 -0600

    Initial version to rename PDF statements from standard mortgage corp
    * Assume PDF statements are downloaded with name "GetDocument.*\.pdf"
    * Rename to "Statement_<date in ISO format>.pdf"

---
+++ rename_standard_mortgage_statements.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import argparse as AP
+import os, os.path, datetime, shutil
+from PyPDF2 import PdfReader
+
+def main():
+    ARGS = get_args()
+    for f in get_files(ARGS.path):
+        statement_date = get_statement_date(f)
+        path_new = os.path.join(
+                os.path.dirname(f), statement_date.strftime("Statement_%Y-%m-%d.pdf"))
+        print(f"rename {f} to {path_new}")
+        shutil.copyfile(f, path_new)
+
+def get_args():
+    ap = AP.ArgumentParser()
+    ap.add_argument("path", help="dir path for the scan to start")
+    return ap.parse_args()
+
+def get_files(path):
+    ret = []
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            if f.startswith("GetDocument") and f.endswith(".pdf"):
+                ret.append(os.path.join(root, f))
+    return ret
+
+def get_statement_date(path):
+    parts = []
+    def pdf_visitor_text_for_statement_date(text, cm, tm, fontDict, fontSize):
+        y = tm[5]
+        x = tm[4]
+        if y > 756 and x > 432:
+            parts.append(text)
+
+    reader = PdfReader(path)
+    page = reader.pages[0]
+    page.extract_text(visitor_text=pdf_visitor_text_for_statement_date)
+    text_body = "".join(parts).strip()
+    print(text_body)
+    return parse_date(text_body)
+
+def parse_date(date_str):
+    return datetime.datetime.strptime(date_str, "%B %d, %Y")
+
+main()
